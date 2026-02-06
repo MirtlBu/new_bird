510b937ae385fc39634a16a3e8ed484c152b0acc
mirtlbu <bubuzoglo@gmail.com>
2026-02-06 09:50:34 +0200

    game over screen added

--- Assets/Scripts/bird_collision.cs
+++ Assets/Scripts/bird_collision.cs
@@ -7,11 +7,37 @@ public class BirdCollision : MonoBehaviour
     public int buildingCounter = 0;
     private GameSpeed gameSpeed;
     public bird_script bird;
+    private bool levelEnding = false;
+    private bool hasEnteredScreen = false;
+
     
     void Start()
     {
         gameSpeed = FindObjectOfType<GameSpeed>();
         bird = GetComponent<bird_script>();
+    }
+    void Update()
+    {
+        if (levelEnding) return;
+        Vector3 viewportPos = Camera.main.WorldToViewportPoint(transform.position);
+
+        if (!hasEnteredScreen)
+        {
+            if (viewportPos.x >= 0 && viewportPos.x <= 1 &&
+                viewportPos.y >= 0 && viewportPos.y <= 1)
+            {
+                hasEnteredScreen = true;
+            }
+        }
+        else
+        {
+            if (viewportPos.x < 0 || viewportPos.x > 1 ||
+                viewportPos.y < 0 || viewportPos.y > 1)
+            {
+                levelEnding = true;
+                SceneManager.LoadScene("game_over");
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -33,7 +59,9 @@ public class BirdCollision : MonoBehaviour
     }
     private IEnumerator EndLevel()
     {
+        levelEnding = true;
         yield return StartCoroutine(bird.LeaveScreen(transform.position));
+
         SceneManager.LoadScene("betweenlevels");
     }
     
